91c0dffb0550aa56a06a82ea85b97ea8a9ac1812
Atharvkadam15 <atharvkadam9996@gmail.com>
2026-08-27 00:20:44 +0530

    Add files via upload

---
+++ Ball_Classification1.py
@@ -0,0 +1,13 @@
+def main():
+  print("Ball Classification Case Study")
+
+  features = [[35,1],[47,1],[90,0],[48,1],[90,0],[35,1],[92,0],[35,1],[35,1],[35,1],[96,0],[43,1],[110,0],[35,1],[95,0]]
+
+  Labels = [1,1,2,1,2,1,2,1,1,1,2,1,2,1,2]
+
+  print("Features are : ",features )
+  print("Labels are : ",Labels )
+
+
+if __name__ == "__main__":
+  main()
